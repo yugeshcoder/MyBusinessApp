acb7d4ca2f276eb9084b4ed3b5b8ea5175b0ba6b
yugeshcoder <yugesh.v.1013@gmail.com>
2024-09-13 13:55:37 +0530

    sucess add

--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -1,37 +1,52 @@
-import { totalparam } from "./Tablebody"
-import { postparam } from "./Header.jsx"
-import { useNavigate } from "react-router-dom"
+import { totalparam } from "./Tablebody";
+import { postparam } from "./Header.jsx";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 const url = import.meta.env.VITE_PORT;
 
-
-
-function handleclick(){
-  const {datePost,qPost,modelPost,compPost} = postparam();
-  let post = {
-    date : datePost,
-    company : compPost,
-    q : qPost,
-    model : modelPost,
-    total : totalparam()
-  }
-  console.log(post)
-  setTimeout(() =>{
-  fetch(`${url}`,{
-    method : 'POST',
-    headers : {"Content-Type" : "application/json"},
-    body : JSON.stringify(post)
-    })
-  }, 1000);
-  setTimeout(() =>{
-    window.location.reload();
-  }, 2000);
-
-}
-
 const Footer = () => {
-  return(
-    <button onClick={handleclick}>SUBMIT</button>
-  )
-}
+  const [done, setDone] = useState(false);
+
+  const handleclick = async () => {
+    const { datePost, qPost, modelPost, compPost } = postparam();
+    let post = {
+      date: datePost,
+      company: compPost,
+      q: qPost,
+      model: modelPost,
+      total: totalparam()
+    };
+    console.log(post);
+
+    setTimeout(async () => {
+      try {
+        const response = await fetch(`${url}`, {
+          method: 'POST',
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(post)
+        });
+
+        if (response.ok) {
+          setDone(true);
+        } else {
+          console.error('Failed to post data');
+        }
+      } catch (error) {
+        console.error('Error posting data:', error);
+      }
+    }, 1000);
+  };
+
+  return (
+    <>
+      {done === false ? (
+        <button onClick={handleclick}>SUBMIT</button>
+      ) : (
+        <h2 style={{ color: "green" }}>Success</h2>
+      )}
+    </>
+  );
+};
 
 export default Footer;
